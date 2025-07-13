eae4e5748c74d59deb92f473a866e9993b9c1574
Hamar93 <hamargabor1993@gmail.com>
2025-07-13 10:00:19 +0200

    Fix import of ProductDetailPageClient

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,4 +1,4 @@
-import { ProductDetailPageClient } from './ProductDetailPageClient'
+import ProductDetailPageClient from './ProductDetailPageClient'
 
 export default function ProductDetailPage({ params }: { params: { id: string } }) {
   return <ProductDetailPageClient id={params.id} />
